58d5e9d60ead70933a32cff79262ee2c711a9c0b
Jesse Sharps <jsharps@jsharps.(none)>
2014-05-11 17:40:41 -0400

    Important but minor change

--- client/routes.js
+++ client/routes.js
@@ -11,7 +11,7 @@ var Router = Backbone.Router.extend({
   loadGame : function(id) {
     Session.set("loading", true)
     Session.set("gameId", id)
-    Session.set("removedTiles", 0)
+    Session.set("gameover", false)
   },
 
   challengeGame : function(id) {
